3f84ce26c0b0cdcf625c45c68af7794567610b7c
Christian Tegnér <christian.tegner@gmail.com>
2014-08-20 19:25:49 +0200

    Working first version

---
+++ index.js
@@ -0,0 +1,46 @@
+var c = require('color');
+var url = require('url');
+
+var input = document.querySelector('input');
+
+function tryParse(color) {
+    try {
+        return c(color);
+    } catch(e) {}
+}
+
+function set(color) {
+    color = color.trim();
+    if (/^\d+$/.test(color)) {
+        color = '#' + color;
+    }
+    var rgb = tryParse(color);
+    if (!rgb) {
+        color = '#' + color;
+        rgb = tryParse(color);
+    }
+    if (!rgb) {
+        return;
+    }
+    document.body.style.backgroundColor = color;
+    input.style.color = rgb.negate().greyscale().lighten(.5).rgbaString();
+    if (!input.value) {
+        input.value = color;
+    }
+    if (color[0] === '#') {
+        color = color.substring(1);
+    }
+    location.hash = encodeURIComponent(color);
+}
+
+input.addEventListener('input', function() {
+    set(input.value);
+});
+
+var hash = url.parse(location.href).hash;
+var hash = hash && hash.substring(1);
+set(decodeURIComponent(hash));
+
+// TODO: Add color details below input
+// TODO: Favicon require('ndarray-canvas')(canvas, red, green, blue)
+
