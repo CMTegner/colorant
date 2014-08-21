119e71c41efdafb8f92239bddaedef1052cc3fe6
Christian Tegnér <christian.tegner@gmail.com>
2014-08-21 22:03:46 +0200

    Add animating favicon; read default color from stylesheet

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var c = require('color');
+var favicon = require('favicon-color');
+var now = require('performance-now');
+var raf = require('raf');
 var url = require('url');
 
+var id;
 var input = document.querySelector('input');
+var icon = document.querySelector('[rel=icon]');
+var current = c(window.getComputedStyle(document.body).backgroundColor);
 
 function tryParse(color) {
     try {
@@ -26,7 +32,7 @@ function set(color) {
         return;
     }
     document.body.style.backgroundColor = color;
-    input.style.color = rgb.negate().greyscale().lighten(0.5).rgbaString();
+    input.style.color = rgb.clone().negate().greyscale().lighten(0.5).rgbaString();
     if (!input.value) {
         input.value = color;
     }
@@ -34,6 +40,24 @@ function set(color) {
         color = color.substring(1);
     }
     location.hash = encodeURIComponent(color.toLowerCase());
+    raf.cancel(id);
+    tweenFavicon(icon, current.rgbArray(), rgb.rgbArray(), 500);
+    current = rgb;
+}
+
+function tweenFavicon(icon, from, to, duration) {
+    var end = now() + duration;
+    var diff = [to[0] - from[0], to[1] - from[1], to[2] - from[2]];
+    id = raf(function loop() {
+        var ratio = Math.min(1, 1 - ((end - now()) / duration));
+        favicon(icon,
+            Math.floor(from[0] + (diff[0] * ratio)),
+            Math.floor(from[1] + (diff[1] * ratio)),
+            Math.floor(from[2] + (diff[2] * ratio)));
+        if (ratio < 1) {
+            id = raf(loop);
+        }
+    });
 }
 
 input.addEventListener('input', function() {
@@ -42,8 +66,11 @@ input.addEventListener('input', function() {
 
 var hash = url.parse(location.href).hash;
 var hash = hash && hash.substring(1);
-set(decodeURIComponent(hash));
+if (hash) {
+    set(decodeURIComponent(hash));
+} else {
+    set(current.keyword() || current.hexString());
+}
 
 // TODO: Add color details below input
-// TODO: Set default input value to default background color
-// TODO: Favicon require('ndarray-canvas')(canvas, red, green, blue)
+// TODO: Improve text color, e.g. #5ad
